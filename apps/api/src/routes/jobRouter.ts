6edddc6984b72ced4c735728040745795f39e692
Gautam Tayal <gautamtayal65@gmail.com>
2025-04-14 17:28:14 +0530

    api nd prisma

--- apps/api/src/routes/jobRouter.ts
+++ apps/api/src/routes/jobRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import YAML from "yaml";
-import { JobSchema } from "@repo/utils"
+import { JobPayload, JobSchema } from "@repo/utils"
 import prisma from "@repo/db/client"
 
 const jobRouter: Router = Router()
@@ -17,6 +17,39 @@ jobRouter.post("/", async(req, res) => {
   } catch (error) {
     console.error(error)
     res.status(400).json({ message: "Invalid job definition" })
+  }
+})
+
+jobRouter.post("/:id/run", async (req, res) => {
+  const jobId = req.params.id
+
+  try {
+    const job = await prisma.job.findUnique({
+      where: {
+        id: jobId
+      }
+    })
+    
+    if (!job) {
+      res.status(404).json({error: "Job not found"})
+      return
+    }
+
+    const payload: JobPayload = {
+      name: job.name,
+      image: job.image,
+      command: job.command,
+      env: job.env as Record<string, string>,
+      timeout: job.timeout ?? undefined,
+      retries: job?.retries ?? undefined,
+    }
+
+    // await jobQueue.add("run-job", payload)
+
+    res.status(201).json({message: "job queued successfully"})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({error: "Failed to queue the order"})
   }
 })
 
